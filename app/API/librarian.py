86f2d1b0402c4bab1b77a00d24f0dbc04504ceb0
Hoodoosoos <paltibor2000@gmail.com>
2025-04-06 20:26:50 +0200

    fixed typos

--- app/API/librarian.py
+++ app/API/librarian.py
@@ -4,22 +4,22 @@ router = APIRouter()
 
 # Rent actions----------------------------
 @router.put("/rentToUser")
-def rent_toUser(rentToUser: librarian.rentToUser):
+def rent_toUser(rentToUser: librarian.RentToUser):
     return "Book rented to User."
 
 @router.put("/rentReturn")
-def rent_return(rentReturn: librarian.rentReturn):
+def rent_return(rentReturn: librarian.RentReturn):
     return "Rented book returned."
 
 @router.put("/rentExtend")
-def rent_extend(rentExtend: librarian.rentExtend):
+def rent_extend(rentExtend: librarian.RentExtend):
     return "Rent extended."
 
 # Fine actions----------------------------
 @router.put("/fineRent")
-def fine_rent(rentFine: librarian.rentFine):
+def fine_rent(rentFine: librarian.FineRent):
     return "Rent fined."
 
 @router.put("/fineDelete")
-def fine_delete(rentDelete: librarian.rentDelete):
+def fine_delete(rentDelete: librarian.FineDelete):
     return "Fine deleted."
