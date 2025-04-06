e90dfd72a458f998622695d6794a6fc020b52f37
Hoodoosoos <paltibor2000@gmail.com>
2025-04-06 20:24:15 +0200

    Created librarian schema and API skeleton

--- app/API/librarian.py
+++ app/API/librarian.py
@@ -1,3 +1,25 @@
 from fastapi import FastAPI, APIRouter
-
+from app.schemas import librarian
 router = APIRouter()
+
+# Rent actions----------------------------
+@router.put("/rentToUser")
+def rent_toUser(rentToUser: librarian.rentToUser):
+    return "Book rented to User."
+
+@router.put("/rentReturn")
+def rent_return(rentReturn: librarian.rentReturn):
+    return "Rented book returned."
+
+@router.put("/rentExtend")
+def rent_extend(rentExtend: librarian.rentExtend):
+    return "Rent extended."
+
+# Fine actions----------------------------
+@router.put("/fineRent")
+def fine_rent(rentFine: librarian.rentFine):
+    return "Rent fined."
+
+@router.put("/fineDelete")
+def fine_delete(rentDelete: librarian.rentDelete):
+    return "Fine deleted."
