72910453725c5f70b8156d332b7c1a3cfb969edc
tarmin0910 <totharmin.pemik@gmail.com>
2025-04-06 20:06:34 +0200

    Admin API
    Új könyv felvitele
    Könyv adatok módosítása
    Könyv törlése

--- app/API/admin.py
+++ app/API/admin.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI, APIRouter
+from app.schemas import admin
 
 router = APIRouter()
+#-----------------Admin book management ------------------
+
+@router.post("/addBook")
+def add_book(newBook: admin.NewBook):
+    return "Book added"
+
+@router.put("/updateBook")
+def update_book(bookUpdate: admin.UpdateBook):
+    return "Book updated"
+
+@router.delete("/deleteBook")
+def delete_book(bookDelete: admin.DeleteBook):
+    return "Book deleted"
+
+#-----------------------------------------------------
