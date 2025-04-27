525a9e423b37d39a7b9794771cd972761f299bfb
tarmin0910 <totharmin.pemik@gmail.com>
2025-04-27 21:56:20 +0200

    Update admin.py

--- app/API/admin.py
+++ app/API/admin.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from app.schemas import admin
+from app.auth import get_current_user
 
 router = APIRouter()
 #-----------------Admin book management ------------------
 
 @router.post("/addBook")
-def add_book(newBook: admin.NewBook):
+def add_book(newBook: admin.NewBook, current_user = Depends(get_current_user)):
+    if current_user["role"] != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can access")
     return "Book added"
 
 @router.put("/updateBook")
-def update_book(bookUpdate: admin.UpdateBook):
+def update_book(bookUpdate: admin.UpdateBook, current_user = Depends(get_current_user)):
+    if current_user["role"] != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can access")
     return "Book updated"
 
 @router.delete("/deleteBook")
-def delete_book(bookDelete: admin.DeleteBook):
+def delete_book(bookDelete: admin.DeleteBook, current_user = Depends(get_current_user)):
+    if current_user["role"] != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can access")
     return "Book deleted"
 
 # -----------------Admin user management ------------------
 
 @router.patch("/updateUserRole")
-def update_user_role(roleUpdate: admin.UpdateUserRole):
+def update_user_role(roleUpdate: admin.UpdateUserRole, current_user = Depends(get_current_user)):
+    if current_user["role"] != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can access")
     return f"User {roleUpdate.user_id} role updated to {roleUpdate.new_role}"
 
 #-----------------------------------------------------
